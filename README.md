bdd5b78ec782eb4dc343eb26f682600352ce6d32
niku <nvdpoel01@gmail.com>
2023-11-07 21:32:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# obsidian-wheel-decide
+When you need a wheel to make decisions for you. Select some lines, press `ctrl+p` and run "Wheel Decide: Create decision wheel". This will open a modal for you with a decision wheel.
+
+<img src="images/screenshot.png" alt="Screenshot" width="500"/>
+
+## Setup
+```bash
+cd /to/your/vault
+mkdir -p .obsidian/plugins
+cd .obsidian/plugins
+
+git clone https://git.cesium.pw/niku/obsidian-wheel-decide.git
+cd obsidian-wheel-decide
+
+npm install
+npm run build
+```
+
+Then you can enable it like any other community plugin.
---
+++ main.ts
@@ -0,0 +1,47 @@
+import { App, Editor, Modal, Plugin } from 'obsidian';
+
+export default class WheelDecide extends Plugin {
+	wheelOptions: string[];
+
+	async onload() {
+		this.addCommand({
+			id: 'create-decision-wheel',
+			name: 'Create decision wheel',
+			editorCallback: (editor: Editor) => {
+				const wheelOptions: string[] = [];
+				const startLine = editor.getCursor("from").line;
+				const endLine = editor.getCursor("to").line;
+				for (let i = startLine; i < endLine + 1; i++) {
+					wheelOptions.push(editor.getLine(i))
+				}
+
+				new WheelModal(this.app, wheelOptions).open()
+			}
+		});
+	}
+}
+
+class WheelModal extends Modal {
+	wheelOptions: string[];
+
+	constructor(app: App, wheelOptions: string[]) {
+		super(app);
+		this.wheelOptions = wheelOptions;
+	}
+
+	onOpen() {
+		const { contentEl } = this;
+		const wheelUrl = new URL("https://wheeldecide.com/e.php?");
+
+		for (let i = 0; i < this.wheelOptions.length; i++) {
+			wheelUrl.searchParams.append(`c${i + 1}`, this.wheelOptions[i])
+		}
+
+		contentEl.innerHTML += `<iframe src="${wheelUrl.href}" width="500" height="500" scrolling="no" frameborder="0"></iframe>`
+	}
+
+	onClose() {
+		const { contentEl } = this;
+		contentEl.empty();
+	}
+}
